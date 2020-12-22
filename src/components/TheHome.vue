c187523fc430d8bb3703d96843e472668657f76b
Wilgenomar <wilgenomarnavarro@gmail.com>
2020-12-22 11:13:10 -0500

    Ultimas modificaciones

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,50 +1,28 @@
 <template>
+  <div class="total">
+    <nav class="navbar navbar-expand-lg navbar-light">  
+      <div class="row collapse navbar-collapse" id="navbarSupportedContent">
+        <form class="form-inline my-2 my-lg-0 offset-md-11">
+          <button
+            class="btn btn-outline-success my-2 my-sm-0 "
+            type="submit"
+            @click.prevent="logOut"
+          >
+            Salir
+          </button>
+        </form>
+      </div>
+    </nav>
 
-<div>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <form class="form-inline my-2 my-lg-0">
-        
-        <button
-          class="btn btn-outline-success my-2 my-sm-0"
-          type="submit"
-          @click.prevent="logOut"
-        >
-          Salir
-        </button>
-      </form>
+    <div class="container mt-5">
+      <div class="card container" style="width: 18rem">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Usuario: {{ user.username }}</li>
+          <li class="list-group-item">Correo: {{ user.email }}</li>
+        </ul>
+      </div>
     </div>
-  </nav>
-
-  <div class="container">
-      <p>
-          {{user.username}}
-
-
-      </p>
-      <p>
-          {{user.email}}
-      </p>
-
-
   </div>
-
-</div>
-  
 </template>
 
 <script>
@@ -56,24 +34,43 @@ export default {
   },
   methods: {
     getUserDetails() {
-        
-        let token = localStorage.getItem('jwt');
-        let user = localStorage.getItem('user');
+      let token = localStorage.getItem("jwt");
+      let user = localStorage.getItem("user");
 
-        if(token){
-            this.user = JSON.parse(user);
-
-        }
-
+      if (token) {
+        this.user = JSON.parse(user);
+      }
     },
-    logOut(){
-        localStorage.removeItem('jwt');
-        localStorage.removeItem('user');
-        this.$router.push('/');
-    }
+    logOut() {
+      localStorage.removeItem("jwt");
+      localStorage.removeItem("user");
+      this.$router.push("/");
+    },
   },
-  created(){
-      this.getUserDetails();
-  }
+  created() {
+    this.getUserDetails();
+  },
 };
 </script>
+
+<style scoped>
+.container {
+  color: #6df3a3;
+}
+.total {
+  background: #34373b;
+}
+
+#navbarSupportedContent{
+  background: #6df3a3;
+}
+
+.navbar{
+  background:  #6df3a3;
+}
+
+.btn{
+  background:   #34373b;
+  color: #6df3a3;
+}
+</style>
